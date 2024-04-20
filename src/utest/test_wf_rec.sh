908b384a048cc680a9ba5e1ffe8c75f10e7737e4
Roadelse <roadelse@HaSee-GX8.com>
2024-04-20 21:52:13 +0800

    minor

--- src/utest/test_wf_rec.sh
+++ src/utest/test_wf_rec.sh
@@ -2,15 +2,12 @@
 
 # export PATH=/home/roadelse/recRoot/GitRepos/workFlowRec/bin:$PATH
 
-
+. wf
 date -d "2023/04/01"
 date
-. wf
 wf_rec da
 if [[ "$(tail -n 1 workflow.md)" == "+ date" ]]; then
     exit 0
 else
     exit 1
 fi
-
-
